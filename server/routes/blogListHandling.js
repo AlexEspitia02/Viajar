4ea9629ca418800cba5c05a4e6449cb669eb6d62
AlexEspitia02 <146508965+AlexEspitia02@users.noreply.github.com>
2024-06-18 14:25:02 +0800

    feat: Added the function of searching blogs by article content keywords

--- server/routes/blogListHandling.js
+++ server/routes/blogListHandling.js
@@ -32,12 +32,36 @@ router.get('/api/blogList/search', async (req, res) => {
     const { keyword } = req.query;
     const regex = new RegExp(keyword, 'i');
 
-    const blogs = await db.collection('posts').find({ title: { $regex: regex } }).toArray();
+    const blogs = await db.collection('posts').aggregate([
+      {
+        $addFields: {
+          "filteredParagraphs": {
+            $filter: {
+              input: "$blocks",
+              as: "block",
+              cond: { $and: [
+                { $eq: ["$$block.type", "paragraph"] },
+                { $regexMatch: { input: "$$block.data.text", regex: regex } }
+              ]}
+            }
+          }
+        }
+      },
+      {
+        $match: {
+          $or: [
+            { title: { $regex: regex } },
+            { "filteredParagraphs": { $ne: [] } }
+          ]
+        }
+      }
+    ]).toArray();
 
     res.status(200).json(blogs);
   } catch (error) {
     res.status(500).json({ error: 'Could not fetch the documents' });
   }
 });
+
 
 module.exports = router;
