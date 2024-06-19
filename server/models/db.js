34702fda090e1313ec92cab3205b14c464728baf
AlexEspitia02 <146508965+AlexEspitia02@users.noreply.github.com>
2024-06-19 16:00:22 +0800

    feat: Deploy mongodb to cloud service (Atlas)

--- server/models/db.js
+++ server/models/db.js
@@ -1,10 +1,15 @@
 const { MongoClient } = require('mongodb');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 let dbConnection;
+const encodedPassword = encodeURIComponent(process.env.MONGODB_PASSWORD);
+let uri = `mongodb+srv://sss88154:${encodedPassword}@mytravel.wveyou6.mongodb.net/?retryWrites=true&w=majority&appName=MyTravel`;
 
 module.exports={
     connectToDb: (cb) => {
-        MongoClient.connect('mongodb://localhost:27017/myTravel')
+        MongoClient.connect(uri)
         .then((client) => {
             dbConnection = client.db();
             return cb();
