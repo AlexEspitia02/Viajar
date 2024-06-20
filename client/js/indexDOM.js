4b5e0d2a3a257b54ed6bb3973e15d52fba5c1b22
AlexEspitia02 <146508965+AlexEspitia02@users.noreply.github.com>
2024-06-20 19:43:20 +0800

    perf: Optimize homepage article list

--- client/js/indexDOM.js
+++ client/js/indexDOM.js
@@ -463,34 +463,42 @@ async function initMap() {
   function displayBlogList(data) {
     const blogListContainer = document.getElementById('information');
     blogListContainer.innerHTML = '';
-    data.forEach((blog) => {
-      const blogBox = document.createElement('div');
-      blogBox.className = 'blogBox'
-      const blogContent = document.createElement('div');
-      blogContent.className = 'blogContent'
-  
-      const blogTitle = document.createElement('div');
-      blogTitle.className = 'blogTitle';
-      blogTitle.innerText = blog.title;
-  
-      blogContent.appendChild(blogTitle);
-      blogBox.appendChild(blogContent);
-  
-      const firstImageBlock = blog.blocks.find(block => block.type === 'image');
-      const imageUrl = firstImageBlock.data.file.url;
-  
-      const blogUrl = document.createElement('a');
-      blogUrl.className = 'blogUrl';
-      blogUrl.href = `/dist/blog.html?id=${blog._id}&title=${encodeURIComponent(blog.title)}`;
-  
-      const blogImg = document.createElement('img');
-      blogImg.src = imageUrl;
-      blogImg.className = 'blogImg';
-      blogUrl.appendChild(blogImg);
-      blogBox.appendChild(blogUrl);
-  
-      blogListContainer.appendChild(blogBox);
-    });
+    const loginButtonBox = document.getElementById('loginButtonBox');
+    loginButtonBox.style.display = 'none'
+    if (loginUserId) {
+      data.forEach((blog) => {
+        const blogBox = document.createElement('div');
+        blogBox.className = 'blogBox'
+        const blogContent = document.createElement('div');
+        blogContent.className = 'blogContent'
+    
+        const blogTitle = document.createElement('div');
+        blogTitle.className = 'blogTitle';
+        blogTitle.innerText = blog.title;
+    
+        blogContent.appendChild(blogTitle);
+        blogBox.appendChild(blogContent);
+    
+        const firstImageBlock = blog.blocks.find(block => block.type === 'image');
+        const imageUrl = firstImageBlock.data.file.url;
+    
+        const blogUrl = document.createElement('a');
+        blogUrl.className = 'blogUrl';
+        blogUrl.href = `/dist/blog.html?id=${blog._id}&title=${encodeURIComponent(blog.title)}`;
+    
+        const blogImg = document.createElement('img');
+        blogImg.src = imageUrl;
+        blogImg.className = 'blogImg';
+        blogUrl.appendChild(blogImg);
+        blogBox.appendChild(blogUrl);
+    
+        blogListContainer.appendChild(blogBox);
+      });
+    }else{
+      const contentDiv = document.getElementById("information");
+      contentDiv.innerHTML = `登入後查看使用者資訊`;
+      contentDiv.style.display = 'flex';
+    }
   }
 
   const inputDiv = document.createElement("div");
